75044dea400a07856cdea920156f08cd18098a8e
Priscila Leite <109990443+PriciLeite@users.noreply.github.com>
2023-03-17 22:00:21 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -3,9 +3,8 @@ using System.Runtime.Serialization;
 
 namespace SalesWebMvc.Services.Exceptions
 {
-    public class DbConcurrencyException : ApplicationException   {
-        
-
+    public class DbConcurrencyException : ApplicationException
+    {
         public DbConcurrencyException(string message) : base(message)
         {
 
